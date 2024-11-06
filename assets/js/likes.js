c5c5a17a31e223c34290059f5318e4e1f0ff375c
Kayky Hyan Nunes <89268311+kayky-nunes31@users.noreply.github.com>
2024-11-06 17:21:14 -0300

    Add files via upload

---
+++ assets/js/likes.js
@@ -0,0 +1,114 @@
+var like1 = 0;
+
+function cont_likes1() {
+    like1 = like1 + 1;
+    foto1.innerHTML = like1;
+}
+var like2 = 0;
+
+function cont_likes2() {
+    like2 = like2 + 1;
+    foto2.innerHTML = like2;
+}
+var like3 = 0;
+
+function cont_likes3() {
+    like3 = like3 + 1;
+    foto3.innerHTML = like3;
+}
+var like4 = 0;
+
+function cont_likes4() {
+    like4 = like4 + 1;
+    foto4.innerHTML = like4;
+}
+var like5 = 0;
+
+function cont_likes5() {
+    like5 = like5 + 1;
+    foto5.innerHTML = like5;
+}
+var like6 = 0;
+
+function cont_likes6() {
+    like6 = like6 + 1;
+    foto6.innerHTML = like6;
+}
+var like7 = 0;
+
+function cont_likes7() {
+    like7 = like7 + 1;
+    foto7.innerHTML = like7;
+}
+var like8 = 0;
+
+function cont_likes8() {
+    like8 = like8 + 1;
+    foto8.innerHTML = like8;
+}
+var like9 = 0;
+
+function cont_likes9() {
+    like9 = like9 + 1;
+    foto9.innerHTML = like9;
+}
+var like10 = 0;
+
+function cont_likes10() {
+    like10 = like10 + 1;
+    foto10.innerHTML = like10;
+}
+var like11 = 0;
+
+function cont_likes11() {
+    like11 = like11 + 1;
+    foto11.innerHTML = like11;
+}
+var like12 = 0;
+
+function cont_likes12() {
+    like12 = like12 + 1;
+    foto12.innerHTML = like12;
+}
+var like13 = 0;
+
+function cont_likes13() {
+    like13 = like13 + 1;
+    foto13.innerHTML = like13;
+}
+var like14 = 0;
+
+function cont_likes14() {
+    like14 = like14 + 1;
+    foto14.innerHTML = like14;
+}
+var like15 = 0;
+
+function cont_likes15() {
+    like15 = like15 + 1;
+    foto15.innerHTML = like15;
+}
+var like16 = 0;
+
+function cont_likes16() {
+    like16 = like16 + 1;
+    foto16.innerHTML = like16;
+}
+var like17 = 0;
+
+function cont_likes17() {
+    like17 = like17 + 1;
+    foto17.innerHTML = like17;
+}
+var like18 = 0;
+
+function cont_likes18() {
+    like18 = like18 + 1;
+    foto18.innerHTML = like18;
+}
+var like19 = 0;
+
+function cont_likes19() {
+    like19 = like19 + 1;
+    foto19.innerHTML = like19;
+}
